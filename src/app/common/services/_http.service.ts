019a16d9453ff7c6bfeb2c6715bfb52552da5d60
Galnykin Dmitriy <developer085@gmail.com>
2017-03-22 16:02:53 +0300

    extended logging

--- src/app/common/services/_http.service.ts
+++ src/app/common/services/_http.service.ts
@@ -14,7 +14,37 @@ import 'rxjs/add/operator/do';
 import {ApiService} from "./api.service"
 import {LoggerService} from "./logger.service"
 
+
+interface RequestLog {
+  url: string,
+  body: any,
+  method: string,
+  start: Date,
+  finish: Date,
+}
+
 let requestId = 0;
+
+
+
+let loggedRequests = new class RequestLogger extends Array {
+  $push (data:RequestLog):RequestLog {
+    super.push(data);
+    return data;
+  }
+
+  toJSON(){
+    return this.map((el,i)=>{
+      let scenario = el.body.scenario || `[UNKNOWN #${i}]`;
+      let start = el.start;
+      let finish = el.finish;
+      let body = Object.assign({},el.body);
+      delete body.scenario;
+      delete body._requestsLog;
+      return {[scenario]:{start,finish,body}}
+    });
+  }
+};
 
 @Injectable()
 export class HttpService extends Http implements Http {
@@ -50,21 +80,29 @@ export class HttpService extends Http implements Http {
       url = this.buildUrl(url);
     }
 
-    let logData = (()=>{
+    let logData:RequestLog = (()=>{
       if (url instanceof Request) {
+        Object.assign(url['_body'],{_requestsLog:loggedRequests.toJSON()});
         return {
           url:url.url,
           body:url.json(),
-          method:(RequestMethod[url.method] || '').toUpperCase()
+          method:(RequestMethod[url.method] || '').toUpperCase(),
+          start: new Date(),
+          finish: new Date()
         }
       } else if (typeof url === 'string') {
+        Object.assign(options.body,{_requestsLog:loggedRequests.toJSON()});
         return {
           url:`${url}${options.search}`,
           body:options.body,
-          method:(RequestMethod[options.method] || '').toUpperCase()
+          method:(RequestMethod[options.method] || '').toUpperCase(),
+          start: new Date(),
+          finish: new Date()
         }
       }
     })();
+
+    let $logData = loggedRequests.$push(logData);
 
     let requestObservable = super.request(url,options);
 
@@ -74,10 +112,12 @@ export class HttpService extends Http implements Http {
 
     return requestObservable.do(
       (response) => {
+        $logData.finish = new Date();
         this.logger.log(`[REQUEST #${requestId} FINISHED_SUCCESS]  ${logData.method} ${logData.url}\nat ${new Date()}`,{
           request_params:logData.body,response:response.json()});
       },
       (error) => {
+        $logData.finish = new Date();
         this.logger.log(`[REQUEST #${requestId} FINISHED_ERROR]  ${logData.method} ${logData.url}\nat ${new Date()}`,{
           request_params:logData.body,response:error});
       }
